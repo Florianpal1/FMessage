6945abe94780cc2cf54de7f8efa081f24e45f29d
flori <florian.fourquenay@outlook.com>
2022-01-12 17:53:52 +0100

    Add copyright

--- FMessageBukkit/src/main/java/fr/florianpal/fmessage/listeners/ChatListener.java
+++ FMessageBukkit/src/main/java/fr/florianpal/fmessage/listeners/ChatListener.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Florianpal
+ *
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Last modification : 12/01/2022 17:42
+ *
+ *  @author Florianpal.
+ */
+
 package fr.florianpal.fmessage.listeners;
 
 
--- FMessageBungee/src/main/java/fr/florianpal/fmessage/managers/DatabaseManager.java
+++ FMessageBungee/src/main/java/fr/florianpal/fmessage/managers/DatabaseManager.java
@@ -1,3 +1,19 @@
+
+/*
+ * Copyright (C) 2022 Florianpal
+ *
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Last modification : 20/10/2021 19:57
+ *
+ *  @author Florianpal.
+ */
+
 package fr.florianpal.fmessage.managers;
 
 import com.zaxxer.hikari.HikariConfig;
